297279f2a9a1c2e4d51ac18067395802037ad244
birkofcz <104515333+birkofcz@users.noreply.github.com>
2023-07-29 14:34:57 +0200

    small changes

--- Experiments/class2.hpp
+++ Experiments/class2.hpp
@@ -6,7 +6,7 @@
 /*   By: sbenes <sbenes@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 11:07:24 by sbenes            #+#    #+#             */
-/*   Updated: 2023/07/26 11:50:25 by sbenes           ###   ########.fr       */
+/*   Updated: 2023/07/29 14:33:21 by sbenes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ class Sample {
 
 public:
 
-	int foo; // declared integer;
+	int foo; // declared integer; ATTRIBUTE
 	
 	Sample(void);  //this is a constructor of a class (no type): <class_name>(parameters);
 	~Sample(void); // this is a destructor of a class (no type): ~<class_name>(parameters);
 
-	void bar(void); //member function prototype
+	void bar(void); //member function prototype - METHOD
 };
 
 #endif
@@ -30,4 +30,4 @@ public:
 /* Destructors release memory space occupied by the objects created by the constructor. 
 In destructor, objects are destroyed in the reverse of object creation.
 
-Constructors and destructors are both special member function of a Class */
+Constructors and destructors are both special member function (methods) of a Class */
