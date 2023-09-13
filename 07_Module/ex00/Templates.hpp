fa1f724343e56c69abce0afdab68e9847d4886e1
Stepan Benes <sbenes@c3r1s1.42prague.com>
2023-09-13 15:41:13 +0200

    ex00 done

--- 07_Module/ex00/Templates.hpp
+++ 07_Module/ex00/Templates.hpp
@@ -6,7 +6,7 @@
 /*   By: sbenes <sbenes@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 13:26:41 by sbenes            #+#    #+#             */
-/*   Updated: 2023/09/13 13:27:28 by sbenes           ###   ########.fr       */
+/*   Updated: 2023/09/13 15:38:47 by sbenes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,47 @@
 
 # include <iostream>
 # include <string>
+
+/* Colors */
+# define GRE "\033[32m"
+# define RED "\033[31m"
+# define YEL "\033[33m"
+# define BLU "\033[34m"
+# define RES "\033[0m"
+
+/* Enumerators */
+enum e_type
+{
+	CHAR	= 0,
+	INT		= 1,
+	FLOAT	= 2,
+	DOUBLE	= 3,
+	INVALID	= 4
+};
+
+/* Functions templates */
+template <typename T>
+void swap(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+	return (a < b)? a : b;
+}
+
+template <typename T>
+T max(T a, T b)
+{
+	return (a > b)? a : b;
+}
+
+/* Functions */
+int identify(char* str);
 
 
 #endif
--- 07_Module/ex00/main.cpp
+++ 07_Module/ex00/main.cpp
@@ -6,14 +6,100 @@
 /*   By: sbenes <sbenes@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 16:20:24 by sbenes            #+#    #+#             */
-/*   Updated: 2023/09/13 13:27:37 by sbenes           ###   ########.fr       */
+/*   Updated: 2023/09/13 15:38:35 by sbenes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Templates.hpp"
+#include <cstdlib>
+#include <iomanip>
+#include <typeinfo>
 
-int main(int ac, char** av)
+int	identify(char *str)
 {
+	std::string s(str);
+	if (s == "char")
+		return CHAR;
+	else if (s == "int")
+		return INT;
+	else if (s == "float")
+		return FLOAT;
+	else if (s == "double")
+		return DOUBLE;
+	else
+		return INVALID;
 
 }
 
+int main(int ac, char** av)
+{
+	if (ac == 4)
+	{
+		int type = identify(av[1]);
+		std::cout << std::endl;
+		switch (type)
+		{
+			case CHAR:
+			{
+				std::cout << "type identified: " << GRE << "char" << RES << std::endl;
+				char a = av[2][0];
+				char b = av[3][0];
+				std::cout << "a = " << a << " [" << typeid(a).name() << "]"<< ", b = " << b << " [" << typeid(b).name() << "]"<< std::endl;
+				std::cout << "calling ::swap(a, b)" << std::endl;
+				::swap(a, b);
+				std::cout << "a = " << a << ", b = " << b << std::endl;
+				std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
+				std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
+				break;
+			}
+			case INT:
+			{
+				std::cout << "type identified: " << GRE << "int" << RES << std::endl;
+				int a = std::atoi(av[2]);
+				int b = std::atoi(av[3]);
+				std::cout << "a = " << a << " [" << typeid(a).name() << "]"<< ", b = " << b << " [" << typeid(b).name() << "]"<< std::endl;
+				::swap(a, b);
+				std::cout << "calling ::swap(a, b)" << std::endl;
+				std::cout << "a = " << a << ", b = " << b << std::endl;
+				std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+				std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+				break;
+			}
+			case FLOAT:
+			{
+				std::cout << std::fixed << std::setprecision(1);
+				std::cout << "type identified: " << GRE << "float" << RES << std::endl;
+				float a = std::atof(av[2]);
+				float b = std::atof(av[3]);
+				std::cout << "a = " << a << " [" << typeid(a).name() << "]"<< ", b = " << b << " [" << typeid(b).name() << "]"<< std::endl;
+				::swap(a, b);
+				std::cout << "calling ::swap(a, b)" << std::endl;
+				std::cout << "a = " << a << ", b = " << b << std::endl;
+				std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+				std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+				break;
+			}
+			case DOUBLE:
+			{
+				std::cout << std::fixed << std::setprecision(1);
+				std::cout << "type identified: " << GRE << "double" << RES << std::endl;
+				double a = std::atof(av[2]);
+				double b = std::atof(av[3]);
+				std::cout << "a = " << a << " [" << typeid(a).name() << "]"<< ", b = " << b << " [" << typeid(b).name() << "]"<< std::endl;
+				::swap(a, b);
+				std::cout << "calling ::swap(a, b)" << std::endl;
+				std::cout << "a = " << a << ", b = " << b << std::endl;
+				std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+				std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+				break;
+			}
+			default:
+				std::cerr << "Invalid type" << std::endl;
+				break;
+		}
+		std::cout << std::endl;
+	}
+	else 
+		return (std::cerr << "Wrong number of arguments" << std::endl, 1);
+	return 0;
+}
