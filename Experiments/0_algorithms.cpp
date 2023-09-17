aba30e6f7792c897f817c4d8e6381b5e085257e2
Stepan Benes <sbenes@c1r1s7.42prague.com>
2023-09-17 13:51:08 +0200

    experimenting with containers and algos

--- Experiments/0_algorithms.cpp
+++ Experiments/0_algorithms.cpp
@@ -6,7 +6,7 @@
 /*   By: sbenes <sbenes@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 17:08:13 by sbenes            #+#    #+#             */
-/*   Updated: 2023/09/16 17:35:52 by sbenes           ###   ########.fr       */
+/*   Updated: 2023/09/17 13:50:44 by sbenes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,6 +78,7 @@ int main()
 
 	std::cout << GRE "\nExamples of algorithms in C++: " RES << std::endl;
 	std::vector<int> vec;
+	std::vector<int>::iterator vi;
 
 	srand(static_cast<unsigned>(time(NULL))); // random number generator seed
 	
@@ -94,6 +95,51 @@ int main()
 	std::cout << "\nstd::sort(vec.begin(), vec.end()): " << std::endl;
 	printVector(vec);
 
+	//find method using iterator
+	vi = std::find(vec.begin(), vec.end(), 3);
+	if (vi != vec.end())
+	{
+		std::cout << "\nstd::find(vec.begin(), vec.end(), 3): finding number three in the vector: " << std::endl;
+		std::cout << *vi << std::endl;
+	}
+
+	std::cout << "\nMax element using *std::max_element(vec.begin(), vec.end()): " << *std::max_element(vec.begin(), vec.end()) << std::endl;
+	std::cout << "Min element using *std::min_element(vec.begin(), vec.end()): " << *std::min_element(vec.begin(), vec.end()) << std::endl;
+
+/* 
+Some more algorithms:
+-- Checking properties on ranges:
+all_of(begin, end, predicate): Returns true if all elements in the range satisfy the predicate.
+any_of(begin, end, predicate): Returns true if any element in the range satisfies the predicate.
+none_of(begin, end, predicate): Returns true if none of the elements in the range satisfy the predicate.
+
+--Counting and finding elements:
+count(begin, end, value): Counts occurrences of a value in a range.
+count_if(begin, end, predicate): Counts occurrences of elements for which predicate is true.
+
+--Modifying sequences:
+copy(begin, end, out_begin): Copies a range to another location.
+move(begin, end, out_begin): Moves a range to another location.
+fill(begin, end, value): Fills a range with a specific value.
+reverse(begin, end): Reverses the order of a range.
+
+--Removing elements:
+remove(begin, end, value): Removes all instances of a value from a range. Note: It doesn't resize the container.
+remove_if(begin, end, predicate): Removes all elements that satisfy the predicate.
+
+--Algorithm on sorted sequences:
+merge(begin1, end1, begin2, end2, out_begin): Merges two sorted ranges.
+set_union(begin1, end1, begin2, end2, out_begin): Computes the union of two sorted ranges.
+set_intersection(begin1, end1, begin2, end2, out_begin): Computes the intersection of two sorted ranges.\
+
+--Complex transformations:
+transform(begin, end, out_begin, unary_function): Applies a function to each element of a range.
+transform(begin1, end1, begin2, out_begin, binary_function): Applies a binary function to elements of two ranges.
+
+
+Initialization:
+iota(begin, end, value): Fills a range with successive increments of the value.
+ */
 	
 
 
